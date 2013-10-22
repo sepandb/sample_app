b42ff26bd574257223a9eaa8eca62f63c1e11bc3
Sepand Barkhodaee <sepand.barkhodaee@gmail.com>
2013-10-22 11:22:52 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,65 +1,53 @@
 require('spec_helper')
 
 describe "StaticPages" do
+
+ subject { page }
  
- describe "Home Page" do
+  describe "Home Page" do
 
- 	it "should have the h1 'Sample App'" do
+ 	before { visit root_path }
 
-	 	visit '/static_pages/home'
-	 	page.should have_selector('h1', :text => 'Sample App')
+ 	it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }  
+ 	it { should_not have_selector('title', text: '| Home') }
 
-	end
+  end
 
-	it "should have the base title" do
-		visit '/static_pages/home'
-	 	page.should have_selector('title',
-	 	 :text => "Rails Sample App")
-	 page.should_not have_selector('title', :text => '| Home')
-	end
-end
 
-	it "should not have a custom page title" do
-		visit '/static_pages/home'
- end
  describe "Help Page" do
+ 	before { visit help_path}
 
- 	it "should have the h1 'Help'" do
+ 	it { should have_selector('h1', text: 'Help') }
+ 	it { should have_selector('title', text: full_title('Help')) }
 
-	 	visit '/static_pages/help'
-	 	page.should have_selector('h1', :text => 'Help')
-	end
-
-	it "should have the right title" do
-		visit '/static_pages/help'
-	 	page.should have_selector('title',
-	 	 :text => "Rails Sample App | Help")
-	end
-end
+  end
  describe "About Page" do
+ 	before { visit about_path}
 
- 	it "should have the h1 'About Us'" do
-
-	 	visit '/static_pages/about'
-	 	page.should have_selector('h1', :text => 'About Us')
-	end
-	it "should have the right title" do
-		visit '/static_pages/about'
-	 	page.should have_selector('title',
-	 	 :text => "Rails Sample App | About Us")
-	end
-end
+ 	it { should have_selector('h1', text: 'About Us') }
+ 	it { should have_selector('title', text: full_title('About Us')) }
+  end
  describe "Contact Page" do
 
- 	it "should have the h1 'Contact'" do
+ 	before {visit contact_path}
 
-	 	visit '/static_pages/contact'
-	 	page.should have_selector('h1', :text => 'Contact')
-	end
-	it "should have the right title" do
-		visit '/static_pages/contact'
-	 	page.should have_selector('title',
-	 	 :text => "Rails Sample App | Contact")
-	end
-end
+ 	it { should have_selector('h1', text: 'Contact') }
+ 	it { should have_selector('title', text: full_title('Contact')) }
+
+  end
+
+  it "should have the right links on the layout" do
+  	visit root_path
+  	click_link "About"
+	page.should have_selector 'title', text: full_title('About Us')
+	click_link "Help"
+	page.should have_selector 'title', text: full_title('Help')
+	click_link "Contact"
+	page.should have_selector 'title', text: full_title('Contact')
+	click_link "Home"
+	click_link "Sign up now!"
+	page.should have_selector 'title', text: full_title('Sign up')
+
+ end
 end
